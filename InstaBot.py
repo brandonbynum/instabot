f7789de9178c540cd05bf2b568d65fa55e86126a
Brandon Bynum <brandonb2124@gmail.com>
2018-11-22 04:31:07 -0700

    Function Cleanup on InstaBot.py

--- InstaBot.py
+++ InstaBot.py
@@ -18,11 +18,17 @@ class InstaBot():
         browser = self.browser
         browser.get('https://instagram.com')
 
-        # Go to login page
-        login_elem = browser.find_element_by_xpath('//a[text() = "Log in"]')
-        login_elem.click()
+        try:
+            # Go to login page
+            login_elem = WebDriverWait(browser, 20).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/article/div[2]/div[2]/p/a')))
+            # login_elem = browser.find_element_by_xpath('//a[text() = "Log in"]')
+            login_elem.click()
 
-        sleep(2)
+            sleep(2)
+        except Exception as e:
+            print(error)
+            exit
 
         # Enter instagram username from console
         # username = input('Username: ')
@@ -56,31 +62,23 @@ class InstaBot():
         except NoSuchElementException:
             pass
 
+    def exists_by_xpath(self, xpath):
+        try:
+            self.browser.find_element_by_xpath(xpath).click()
+        except NoSuchElementException:
+            pass
+        return True
 
     def closeAppOverlays(self):
-        browser = self.browser
-        # Close Notification Overlay
-        try:
-            browser.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
-        except NoSuchElementException:
-            pass
+        overlays = (
+            '/html/body/div[4]/div/div/div/div[3]/button[2]', # 'Turn on Notifications' Overlay
+            '/html/body/div[2]/div/div/div/div[1]/button',    # 'Get App' Overlay
+            '//*[@id="react-root"]/section/div/span'          # 'Get App' Overlay at bottom of screen
+        )
+        for i in range(3):
+            self.exists_by_xpath(overlays[i])
 
-        # Close 'get app' overlay if present
-        try:
-            browser.find_element_by_class_name('ckWGn').click()
-        except NoSuchElementException:
-            pass
 
-        # Allow page time to load
-        sleep(2)
-
-        # Close bottom app ad
-        try:
-            browser.find_element_by_class_name('uDNXD').click()
-        except NoSuchElementException:
-            pass
-
-        sleep(2)
 
     def like_hashtags(self, hashtag):
         browser = self.browser
@@ -88,8 +86,15 @@ class InstaBot():
         sleep(2)
         for i in range(1,3):
             browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
         links = browser.find_elements_by_tag_name('a')
+
         post_links = [elem.get_attribute('href') for elem in links]
+        print('post_links1:\n\t')
+        for i, link in enumerate(post_links):
+            print('\tLink #' + str(i + 1) +': ' + str(link))
+        print('-----------------------------')
+
         post_links = [href for href in post_links if hashtag in href]
         print('#' + hashtag + ' photos: ' + str(len(post_links)))
         for i, link in enumerate(post_links):
@@ -101,10 +106,14 @@ class InstaBot():
 username = ''
 password = ''
 myIGBot = InstaBot(username, password)
+print('Logging in...')
 myIGBot.login()
 sleep(2)
+print('Checking for verification...')
 myIGBot.verificationCheck()
-sleep(2)
+sleep(1)
+print('Checking for overlays...')
 myIGBot.closeAppOverlays()
-sleep(5)
-myIGBot.like_hashtags('skateboarding')
+print("Overlays closed.")
+sleep(1)
+#myIGBot.like_hashtags('skateboarding')
