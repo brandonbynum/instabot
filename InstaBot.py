066e3892189ee45648fed5e433f8a9526036f98e
Brandon Bynum <brandonb2124@gmail.com>
2018-11-28 21:12:41 -0700

    Extended login verification support and extended terminal menu options and activity status displayed.

--- InstaBot.py
+++ InstaBot.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import getpass
+import sys
 
 
 class InstaBot():
@@ -13,24 +14,39 @@ class InstaBot():
         self.password = password
         self.browser = webdriver.Chrome()
 
+
+    # Checks if an element exists by searching for it's expected xpath
+    def exists_by_xpath(self, xpath):
+        try:
+            self.browser.find_element_by_xpath(xpath)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def closeProgram(self, reason):
+        print(reason + '\nClosing InstaBot. GoodBye.')
+        self.browser.quit()
+        sys.exit()
+
+
     def login(self):
-        # starting a new browser session
+        # Starting a new browser session / driver.
         browser = self.browser
         browser.get('https://instagram.com')
 
         try:
-            # Go to login page
+            # Go to login page.
             login_elem = WebDriverWait(browser, 20).until(
             EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/article/div[2]/div[2]/p/a')))
             # login_elem = browser.find_element_by_xpath('//a[text() = "Log in"]')
             login_elem.click()
+            print('Logging in...')
 
             sleep(2)
         except Exception as e:
-            print(error)
-            exit
+            self.closeProgram('Wrong password entered.')
 
-        # Enter instagram username from console
+        # Enter instagram username from console.
         # username = input('Username: ')
         # browser.find_element_by_name('username').send_keys(username)
 
@@ -39,35 +55,65 @@ class InstaBot():
         # browser.find_element_by_name('password').send_keys(password)
 
         # Enter Username and Password
-        username  = self.username
-        browser.find_element_by_name('username').send_keys(username)
+        usernameElement = browser.find_element_by_name('username')
+        usernameElement.clear()
+        usernameElement.send_keys(self.username)
 
-        password = self.password
-        browser.find_element_by_name('password').send_keys(password)
+        passwordElement = browser.find_element_by_name('password')
+        passwordElement.clear()
+        passwordElement.send_keys(self.password)
+        sleep(1)
 
         # Click login button
         browser.find_element_by_xpath('//button[text()="Log in"]').click()
+        sleep(3)
+
+        if self.exists_by_xpath('//*[@id="react-root"]/section/div/div/div[3]/form/span/button[text()="Send Security Code"]'):
+            print('Checking for verification...')
+            self.verificationCheck()
+        # Check if the login has been successful, exit program otherwise.
+        elif self.exists_by_xpath('//*[@id="react-root"]/section/main/section/div[3]/div[1]/div/div[2]/div[1]/a[text()=' + username +']'):
+            print('Login Successs')
+        else:
+            self.closeProgram('Password was incorrect. Please check password before running again.')
+
 
     def verificationCheck(self):
         browser = self.browser
+        attempts = 5
+
         # Check if verification code is needed.
         # If so, enter the code in terminal.
         try:
             browser.find_element_by_xpath('//*[@id="react-root"]/section/div/div/div[3]/form/span/button[text()="Send Security Code"]').click()
-            print('A security code is required to login. Please check your email for the code and enter it.')
-            code = input('Code: ')
-            # Input Key and Submit
-            browser.find_element_by_xpath('//*[@id="security_code"]').send_keys(code)
-            browser.find_element_by_xpath('//*[@id="react-root"]/section/div/div/div[2]/form/span/button').click()
+            print('A security code is required to login. Please check your email for the code and enter the code below. (' + str(attempts) + ' attempts remaining)')
+
+            # Loop giving 5 attempts to correctly enter code.
+            for i in range(5):
+                code = input('Code: ')
+                # Input Key and Submit
+                codeElement = browser.find_element_by_xpath('//*[@id="security_code"]')
+                codeElement.send_keys(code)
+
+                # Submit verification code
+                browser.find_element_by_xpath('//*[@id="react-root"]/section/div/div/div[2]/form/span/button').click()
+                sleep(1)
+
+                # If code is wrong, reduce allowed attempts otherwise continue.
+                if self.exists_by_xpath('//*[@id="form_error"]/p'):
+                    attempts -= 1
+                    if attempts == 0:
+                        self.closeProgram('Too many failed verification code attempts.')
+                    else:
+                        print('Wrong code, try again. (' + str(attempts) + ' attempts remaining)')
+                        codeElement.clear()
+                else:
+                    break
+
         except NoSuchElementException:
             pass
 
-    def exists_by_xpath(self, xpath):
-        try:
-            self.browser.find_element_by_xpath(xpath).click()
-        except NoSuchElementException:
-            pass
-        return True
+
 
     def closeAppOverlays(self):
         overlays = (
@@ -75,13 +121,20 @@ class InstaBot():
             '/html/body/div[2]/div/div/div/div[1]/button',    # 'Get App' Overlay
             '//*[@id="react-root"]/section/div/span'          # 'Get App' Overlay at bottom of screen
         )
+        print('Checking for overlays...')
         for i in range(3):
-            self.exists_by_xpath(overlays[i])
+            overlay = overlays[i]
+            if self.exists_by_xpath(overlay):
+                self.browser.find_element_by_xpath(overlay).click()
+                print("Overlays closed.")
+            else:
+                print("No Overlays found.")
 
 
 
     def like_hashtags(self, hashtag):
         browser = self.browser
+        print("Searching hashtag '" + hashtag + "'")
         browser.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
         sleep(2)
         for i in range(1,3):
@@ -106,14 +159,11 @@ class InstaBot():
 username = ''
 password = ''
 myIGBot = InstaBot(username, password)
-print('Logging in...')
+
 myIGBot.login()
-sleep(2)
-print('Checking for verification...')
+
 myIGBot.verificationCheck()
 sleep(1)
-print('Checking for overlays...')
-myIGBot.closeAppOverlays()
-print("Overlays closed.")
+#myIGBot.closeAppOverlays()
 sleep(1)
 #myIGBot.like_hashtags('skateboarding')
