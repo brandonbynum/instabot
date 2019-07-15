d20b949996038b41c4d4cfbdc40160f46aee21c7
Brandon Bynum <brandonb2124@gmail.com>
2019-07-14 20:29:25 -0700

    Structure update

---
+++ config.py
@@ -0,0 +1,9 @@
+USERNAME = 'munybrr'
+
+PASSWORD = ''
+
+LIKE_FEED = False
+
+LIKE_HASHTAGS = True
+
+HASHTAGS = ['nightwalkermagazine', 'yashicat4', 'everybodyfilm']
--- run.py
+++ run.py
@@ -1,17 +1,25 @@
-from InstaBot import InstaBot
+import InstaBot
+import config
+
+import getpass
+from time import sleep
 
 
 print("Welcome to Instabot!")
 
 print("Currently the only supported feature is liking images based on hash tag searches.\n")
-print("Please enter a hashtag or list of hashtags you would like to search separated by commas, then press 'Enter'. \nEx. 'skateboarding, basketball, shoes'.\n")
-hashtags = input()
-hashtags = [hashtag.strip() for hashtag in hashtags.split(',')]
+#print("Please enter a hashtag or list of hashtags you would like to search separated by commas, then press 'Enter'. \nEx. 'skateboarding, basketball, shoes'.\n")
+#hashtags = input()
+
+hashtags = config.HASHTAGS
 
 username = input('\nTime to log in. \nPlease enter your username: ')
 password = getpass.getpass('Password:')
 
-myIGBot = Instabot.InstaBot()
+#username = config.username
+#password = config.password
+
+myIGBot = InstaBot.InstaBot()
 myIGBot.login(username, password)
 sleep(1)
 links = myIGBot.get_hashtag_links(hashtags)
